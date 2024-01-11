4c2cd239863178b1f3064a6468373d6ca441b8ff
Andika <litageming@gmail.com>
2024-01-11 20:46:48 +0700

    my crush XII RPL

--- app/Views/Dasboard.php
+++ app/Views/Dasboard.php
@@ -43,6 +43,7 @@
   <div class="preloader flex-column justify-content-center align-items-center">
 <img class="animation__shake" src="<?= base_url('assets/dist/img/AdminLTELogo.png') ?>" alt="AdminLTELogo" height="60" width="60">
 
+
   </div>
 
   <!-- Navbar -->
@@ -187,7 +188,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.php" class="brand-link">
-      <img src="<?= base_url('dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8') ?>">
+      <img src="<?= base_url('assets/dist/img/AdminLTELogo.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -196,7 +198,9 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url('dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image') ?>">
+<!-- Assuming your 'assets' folder is at the root level of your project -->
+<img src="<?= base_url('assets/dist/img/user2-160x160.jpg') ?>" class="img-circle elevation-2" alt="User Image">
+
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
